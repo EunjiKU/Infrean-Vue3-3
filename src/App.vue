3248819ca5d1504a987e01c5386c176c7f7af652
KUEUNJI <yoo128@easymedia.net>
2023-03-28 17:29:27 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="텍스트 변경 테스트"/>
   </div>
 </template>
 
