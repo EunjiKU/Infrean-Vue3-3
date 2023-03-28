ab8449f5e01d39e1ab6c8fc5da023cd8f1141793
KUEUNJI <yoo128@naver.com>
2023-03-29 03:17:19 +0900

    이벤트 버스 시작

--- src/views/AskVeiw.vue
+++ src/views/AskVeiw.vue
@@ -1,18 +1,6 @@
 <template>
   <div>
     <ListItem></ListItem>
-    <!-- <div>
-      <ul class="list_item">
-        <li class="item" v-for="item in fetchedAsk" v-bind:key="item">
-          <router-link class="tit" :to="`/item/${item.id}`">{{ item.title }}</router-link>
-          <p class="txts">
-            <span class="txt">💰{{ item.points }}</span>
-            <router-link class="txt" v-bind:to="`/user/${item.user}`">😊{{ item.user }}</router-link>
-            <span class="txt">⏰{{ item.time_ago }}</span>
-          </p>
-        </li>
-      </ul>
-    </div> -->
   </div>
 </template>
 
@@ -22,6 +10,9 @@ import ListItem from '@/components/ListItem.vue'
 export default {
   components: {
     ListItem,
+  },
+  created() {
+    this.$store.dispatch("FETCH_ASK")
   }
 }
 </script>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,18 +1,6 @@
 <template>
   <div>
     <ListItem></ListItem>
-    <!-- <div>
-      <ul class="list_item">
-        <li class="item" v-for="item in fetchedJobs" v-bind:key="item">
-          <a class="tit" v-bind:href="item.url">{{ item.title }}</a>
-          <p class="txt">
-            <span class="txt">💰{{ item.points || 0 }}</span>
-            <span class="txt">😊{{ item.domain || "no" }}</span>
-            <span class="txt">⏰{{ item.time_ago }}</span>
-          </p>
-        </li>
-      </ul>
-    </div> -->
   </div>
 </template>
 
@@ -22,6 +10,9 @@ import ListItem from '@/components/ListItem.vue'
 export default {
   components: {
     ListItem,
+  },
+  created() {
+    this.$store.dispatch("FETCH_JOBS")
   }
 }
 </script>
