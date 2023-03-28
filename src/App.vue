1bf9c189e581a6bd7512ac6bba76e6ef11ab0365
KUEUNJI <yoo128@easymedia.net>
2023-03-28 17:48:26 +0900

    테스ㅌ

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="텍스트 변경 테스트입니다"/>
+    <p>안녕</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
